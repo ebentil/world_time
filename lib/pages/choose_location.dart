e58c0938d865f99397ae9d6824611d4d864a64eb
Ebo <bentilebo@gmail.com>
2020-10-02 20:32:32 +0000

    #26 flutter package http

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,31 +6,6 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  void getData() async {
-    //simulate a network request for username
-    // await Future.delayed(Duration(seconds: 3), () {
-    //   print('eden');
-    // });
-
-    //same as above, we are just assigning the value to the name variable
-    String name = await Future.delayed(Duration(seconds: 3), () {
-      return 'eden';
-    });
-
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-      return 'not your averge baby boy';
-    });
-
-    print('$name - $bio');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print('hey there');
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
