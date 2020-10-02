01a8c76f88c8c14677a48ff30f1b2ba0050f8d8f
Ebo <bentilebo@gmail.com>
2020-10-02 20:57:35 +0000

    #27 wordtimeapi,substring,parse str as int,timeadd

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -8,18 +8,29 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
+  void getTime() async {
+    //make request
     Response response =
-        await get('https://jsonplaceholder.typicode.com/todos/1');
+        await get('http://worldtimeapi.org/api/timezone/Africa/Accra');
     Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+    // print(data);
+
+    // get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    // print(datetime);
+    print(offset);
+
+    // Create Datetime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
